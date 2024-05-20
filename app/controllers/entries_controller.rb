c4db35f1eae746a4575f0acbb94493f9600e8758
fernandahonda <fernanda.honda@kellogg.northwestern.edu>
2024-05-20 02:38:26 +0000

    date adjustment

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -14,7 +14,7 @@ class EntriesController < ApplicationController
     @entry = Entry.new
     @entry["title"] = params["title"]
     @entry["description"] = params["description"]
-    @entry["posted_on"] = Date.today
+    @entry["posted_on"] = params["posted_on"]
     @entry["place_id"] = params["place_id"]
     @entry.save
     redirect_to "/places/" + params["place_id"]
